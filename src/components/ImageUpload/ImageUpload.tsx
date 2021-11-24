48947ff0684e82abb72324b994dc2ef7bc94b58e
sangah <sangah0222@naver.com>
2021-11-24 17:57:00 +0900

    [Add] frame

---
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -0,0 +1,11 @@
+import "./ImageUpload.scss";
+
+const ImageUpload = () => {
+  return (
+    <>
+      <div className="upload">hi</div>
+    </>
+  );
+};
+
+export default ImageUpload;
--- src/pages/TakePicturePage/TakePicturePage.tsx
+++ src/pages/TakePicturePage/TakePicturePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import MainBackground from "../components/Background/MainBackground";
-import { TakePicture } from "../components";
+import MainBackground from "../../components/Background/MainBackground";
+import { TakePicture } from "../../components";
 const TakePicturePage = () => {
   return (
     <MainBackground>
