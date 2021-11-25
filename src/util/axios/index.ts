82b64eae0ff6031de67fab7e5434d54d22b189eb
LeeJinWoo <wlsdn5793@gmail.com>
2021-11-25 23:26:17 +0900

    [ADD] sound file

--- src/util/axios/index.ts
+++ src/util/axios/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = 'http://118.67.143.47:3000'
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/http://118.67.143.47:3000'
+
 export const getRequest = () => {
   const request = axios.create({
     timeout: 10000,
